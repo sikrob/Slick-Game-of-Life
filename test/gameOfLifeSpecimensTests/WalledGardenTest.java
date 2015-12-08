44f5536f8dc8114c3863d38dcb2339ec26e48706
Robert Sikorski <sikorski.rob@gmail.com>
2015-12-08 16:21:21 -0600

    Adding test to demonstrate setting initial states.

--- test/gameOfLifeSpecimensTests/WalledGardenTest.java
+++ test/gameOfLifeSpecimensTests/WalledGardenTest.java
@@ -17,5 +17,21 @@ public class WalledGardenTest {
 			assertNotNull("WalledGarden initilization should throw exception on non-positive dimensions passed.", e);
 		}
 	}
+
+	@Test
+	public void testSettingInitialStateChangesSpecimens() {
+		int[][] liveList = new int[1][2];
+		liveList[0][0] = 0;
+		liveList[0][1] = 0;
+		
+		try {
+			WalledGarden garden = new WalledGarden(1,1);
+			assertFalse("Prior to setting initial state, specimens are not alive.", garden.specimenAliveAt(0, 0));
+			garden.setInitialState(liveList);
+			assertTrue("Setting initial state should cause the state to change to alive.", garden.specimenAliveAt(0, 0));
+		} catch (Exception e) {
+			fail("testSettingInitialStateChangesSpecimens exception occured: " + e.getMessage());
+		}
+	}
 	
 }
